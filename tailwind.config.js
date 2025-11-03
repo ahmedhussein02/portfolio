0c14bb6a2e41458c31c0ffb08ae1562d9c664f6f
ahmedhussein02 <ahmedhussein.root@gmail.com>
2025-11-03 14:30:05 +0200

    Initial commit - Flask portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js,py}",
+    "./src/**/*.{css,js}",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
